2784d655647f046a3d00d9dd930c0a6f91a0c556
sGentiletti <gentiletti.sergio@gmail.com>
2022-07-29 00:37:32 -0300

    desafio 2

--- frontend/js/favoritas.js
+++ frontend/js/favoritas.js
@@ -5,34 +5,53 @@ window.onload = () => {
   app.appendChild(container);
 
   // Aqui debemos agregar nuestro fetch
+  fetch("http://localhost:3031/api/movies")
+    .then((res) => res.json())
+    .then((peliculas) => {
+      const data = peliculas.data;
+      const obj = Object.fromEntries(
+        data.map((ele) => [
+          ele.id,
+          {
+            ele,
+          },
+        ])
+      );
 
+      let peliculasFavoritas = [];
 
-
-
-    let data = peliculas.data;
-
-    data.forEach((movie) => {
-      const card = document.createElement("div");
-      card.setAttribute("class", "card");
-
-      const h1 = document.createElement("h1");
-      h1.textContent = movie.title;
-
-      const p = document.createElement("p");
-      p.textContent = `Rating: ${movie.rating}`;
-
-      const duracion = document.createElement("p");
-      duracion.textContent = `Duración: ${movie.length}`;
-
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-      if (movie.genre !== null) {
-        const genero = document.createElement("p");
-        genero.textContent = `Genero: ${movie.genre.name}`;
-        card.appendChild(genero);
+      if (sessionStorage.getItem("favoritos")) {
+        const idsPeliculasFavoritas = JSON.parse(
+          sessionStorage.getItem("favoritos")
+        );
+        idsPeliculasFavoritas.forEach((id) => {
+          let pelicula = obj[id];
+          peliculasFavoritas.push(pelicula);
+        });
       }
-      card.appendChild(duracion);
+
+      peliculasFavoritas.forEach((movie) => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+        const h1 = document.createElement("h1");
+        h1.textContent = movie.ele.title;
+
+        const p = document.createElement("p");
+        p.textContent = `Rating: ${movie.ele.rating}`;
+
+        const duracion = document.createElement("p");
+        duracion.textContent = `Duración: ${movie.ele.length}`;
+
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+        if (movie.ele.genre !== null) {
+          const genero = document.createElement("p");
+          genero.textContent = `Genero: ${movie.ele.genre.name}`;
+          card.appendChild(genero);
+        }
+        card.appendChild(duracion);
+      });
     });
-  
 };
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,81 +3,90 @@ window.onload = () => {
   const container = document.createElement("div");
   container.setAttribute("class", "container");
   app.appendChild(container);
-  
-  console.log("hola clase")
-  
-  
-  
+
+  console.log("hola clase");
+
   // Aqui debemos agregar nuestro fetch
-  
+
   fetch("http://localhost:3031/api/movies")
-  .then(res => res.json())
-  .then(informacion =>{
+    .then((res) => res.json())
+    .then((informacion) => {
+      console.log("🚀 ~ file: main.js ~ line 16 ~ informacion", informacion);
 
-  console.log("🚀 ~ file: main.js ~ line 16 ~ informacion", informacion)
-  
-  let movies = informacion.data
-  
-  movies.forEach(movie => {
-    
-    const id = movie.id
-    const h2 = movie.title;
-    const img = movie.image[0].url;
-    const genre = movie.genre?.name;
-    const date = movie.release_date;
+      let movies = informacion.data;
 
-    const test =
-    
-    `<div class="card" style="width: 18rem;">
+      movies.forEach((movie) => {
+        const id = movie.id;
+        const h2 = movie.title;
+        const img = movie.image[0].url;
+        const genre = movie.genre?.name;
+        const date = movie.release_date;
+
+        const test = `<div class="card" style="width: 18rem;">
     <img class="card-img-top" src="${img}" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">${h2}</h5>
       <p class="card-text">${genre}</p>
       <p class="card-text">${date}</p>
-      <a href="C:/Users/Leo/Desktop/frontend/formulario.html?id=${id}" class="btn btn-primary">Go somewhere</a>
+      <a href="C:/Users/Sergio/Desktop/DH/clase40/frontend/formulario.html?id=${id}" class="btn btn-primary">Go somewhere</a>
+      <div class="favorito" data-id="${id}">⭐</div>
     </div>
-  </div>`
+  </div>`;
 
-    container.insertAdjacentHTML("beforeend", test)
+        container.insertAdjacentHTML("beforeend", test);
+      });
 
-  });
-  
-})
+      const estrellas = document.querySelectorAll(".favorito");
 
+      estrellas.forEach((estrella) => {
+        estrella.addEventListener("click", (e) => {
+          const idPeliculaFav = +e.target.dataset.id;
+          const sessionFavoritos =
+            JSON.parse(sessionStorage.getItem("favoritos")) ?? "";
+          const esteIdEsRepetido = sessionFavoritos.includes(idPeliculaFav);
 
+          if (esteIdEsRepetido) return;
 
+          if (sessionFavoritos) {
+            sessionFavoritos.push(idPeliculaFav);
+            sessionStorage.setItem(
+              "favoritos",
+              JSON.stringify(sessionFavoritos)
+            );
+          } else {
+            sessionStorage.setItem("favoritos", `[${idPeliculaFav}]`);
+          }
 
-
-
-
-
+          e.target.classList.add("disabled");
+        });
+      });
+    });
 
   //  Codigo que debemos usar para mostrar los datos en el frontend
 
-    // let data = peliculas.data;
+  // let data = peliculas.data;
 
-    // data.forEach((movie) => {
-    //   const card = document.createElement("div");
-    //   card.setAttribute("class", "card");
+  // data.forEach((movie) => {
+  //   const card = document.createElement("div");
+  //   card.setAttribute("class", "card");
 
-    //   const h1 = document.createElement("h1");
-    //   h1.textContent = movie.title;
+  //   const h1 = document.createElement("h1");
+  //   h1.textContent = movie.title;
 
-    //   const p = document.createElement("p");
-    //   p.textContent = `Rating: ${movie.rating}`;
+  //   const p = document.createElement("p");
+  //   p.textContent = `Rating: ${movie.rating}`;
 
-    //   const duracion = document.createElement("p");
-    //   duracion.textContent = `Duración: ${movie.length}`;
+  //   const duracion = document.createElement("p");
+  //   duracion.textContent = `Duración: ${movie.length}`;
 
-    //   container.appendChild(card);
-    //   card.appendChild(h1);
-    //   card.appendChild(p);
-    //   if (movie.genre !== null) {
-    //     const genero = document.createElement("p");
-    //     genero.textContent = `Genero: ${movie.genre.name}`;
-    //     card.appendChild(genero);
-    //   }
-    //   card.appendChild(duracion);
-    // });
-
+  //   container.appendChild(card);
+  //   card.appendChild(h1);
+  //   card.appendChild(p);
+  //   if (movie.genre !== null) {
+  //     const genero = document.createElement("p");
+  //     genero.textContent = `Genero: ${movie.genre.name}`;
+  //     card.appendChild(genero);
+  //   }
+  //   card.appendChild(duracion);
+  // });
 };
